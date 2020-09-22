0e3f31fa6631f01a9dd148efb99df448c92d18cf
Hirotaka Hachiya <hhachiya@wakayama-u.ac.jp>
2020-09-23 00:38:25 +0900

    add cseft

--- set_matching_example.py
+++ set_matching_example.py
@@ -12,19 +12,25 @@ import copy
 import pickle
 
 #----------------------------
-# パラメータの作成
+# set parameters
 isTrain = True
 
-isSelfAttention = True
+# flag of self-attention and cseft
+isAttention = True
 
-# ベースチャネル数
+# base channel numbers of CNN layer
 baseChn = 32
 
-# データの作成
+# flag of making data
 isMakeData = 0
 
-# データの種類
-dataMode = 2 #順不同
+# batch size
+batch_size = 10
+
+epochs = 3
+
+# set data mode
+dataMode = 2 # 1: reverse the order, 2: fix the order
 
 if dataMode == 1:
     dataFileName = 'MNIST_group_permutate.pkl'
@@ -35,21 +41,21 @@ gID = np.array([[1,3],[2,4]])
 #----------------------------
 
 #----------------------------
-# データの作成
-# 画像サイズ（高さ，幅，チャネル数）
+# make data
+# image size (height, weidth, channels)
 H, W, C = 28, 28, 1
 
 if isMakeData:
-    # MNISTデータの読み込み
+    # load MNIST data
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-    # アイテム数が２つのグループを作成
+    # make two different sets containing two images (items) of 2&4 or 1&3
     def makeGroup(X1,X2,dataMode=1):
         if dataMode == 1:
-            #半分はX1とX2の順番を変える
+            # reverse the order of images (items)
             X = np.array([np.concatenate([X1[[i]],X2[[i]]],axis=0) if i%2==0 else np.concatenate([X2[[i]],X1[[i]]],axis=0) for i in range(np.min([len(X1),len(X2)]))]) 
         elif dataMode == 2:
-            # 順番固定
+            # fix the order of images (items)
             X = np.array([np.concatenate([X1[[i]],X2[[i]]],axis=0) for i in range(np.min([len(X1),len(X2)]))]) 
         return X
 
@@ -78,88 +84,203 @@ if isMakeData:
         pickle.dump(y_test,fp)
 
 else:
-    # MNISTデータの読み込み
+    # load data from pickle file
     with open(dataFileName,'rb') as fp:
         x_train = pickle.load(fp)
         y_train = pickle.load(fp)
         x_test = pickle.load(fp)
         y_test = pickle.load(fp)
 
-# 集合の中のアイテム数（固定でいいのか？）
+
+# the number of items in a set (NOTE that the number is FIXED)
 nItem = x_train.shape[1]
 
-# 画像の正規化
+# normalize images
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
-# （データ数，高さ，幅，チャネル数）にrehspae
+# reshape to（N，H，W，C）
 x_train = x_train.reshape(x_train.shape[0], nItem, H, W, C)
 x_test = x_test.reshape(x_test.shape[0], nItem, H, W, C)
 #----------------------------
 
-#----------------------------
-# CS function to compute cross set matching scores
-class cross_set_score(tf.keras.layers.Layer):
-    def __init__(self,dim=20, nHead=1):
-        super(cross_set_score, self).__init__()
-        self.dim = dim
-        self.nHead = nHead
+# #----------------------------
+# # multi-head CS function to make cros-set matching score map
+# class cross_set_score_old(tf.keras.layers.Layer):
+#     def __init__(self, head_size=20, num_heads=2):
+#         super(cross_set_score_old, self).__init__()
+#         self.head_size = head_size
+#         self.num_heads = num_heads
 
-        # multi-head linear function, l(x|W_0), l(x|W_1)...l(x|W_nHead) for each item feature vector x.
-        # one big linear function with weights of W_0, W_1, ..., W_nHead outputs dim*nHead-dim vector
-        self.linear = tf.keras.layers.Dense(units=self.dim*self.nHead,activation='relu',use_bias=False)
+#         # multi-head linear function, l(x|W_0), l(x|W_1)...l(x|W_num_heads) for each item feature vector x.
+#         # one big linear function with weights of W_0, W_1, ..., W_num_heads outputs head_size*num_heads-dim vector
+#         self.linear = tf.keras.layers.Dense(units=self.head_size*self.num_heads,activation='relu',use_bias=False)
+
+#         # linear function to combine multi-head score maps
+#         self.conv = tf.keras.layers.Conv2D(filters=1, strides=(1,1), padding='same', kernel_size=(1,1))
+
+#     def call(self, x):
+
+#         # linear transofrmation (nSet*nItem,head_size*num_heads)
+#         x = self.linear(x)
+
+#         # reshape (nSet*nItem,head_size*num_heads) to (num_heads, nSet*nItem,head_size)
+#         x = tf.transpose(tf.reshape(x,[-1,self.num_heads,self.head_size]),[1,0,2])
+
+#         # inner products between all pairs of items, outputing (num_heads, nSet*nItem, nSet*nItem)-score map
+#         xx = tf.matmul(x,tf.transpose(x,[0,2,1]))/tf.sqrt(tf.cast(self.head_size,tf.float32))
+
+#         # reshape (num_heads, nSet*nItem, nSet*nItem,1)
+#         xx = tf.expand_dims(xx,-1)
+
+#         # sum up score-map in each block of size (nItem, nItem) using conv2d with weights of ones
+#         # outputing (num_heads, nSet, nSet, 1)-score map
+#         scores = tf.keras.layers.Conv2D(filters=1,strides=(nItem,nItem),kernel_size=(nItem,nItem),
+#                     trainable=False,use_bias=False,weights=[tf.ones((nItem,nItem,1,1))])(xx)
+
+#         # devided by the two numbers of items of two sets（NOTE that nItem is SIMPLY fixed）
+#         scores = scores/nItem/nItem
+
+#         # linearly combine multi-head score maps
+#         # reshape (num_heads, nSet, nSet, 1) to (1, nSet, nSet, num_heads)
+#         scores = tf.transpose(scores,[3,1,2,0])
+#         scores = self.conv(scores)
+
+#         return scores[0,:,:,0]
+# #----------------------------
+
+#----------------------------
+# multi-head CS function to make cros-set matching score map
+class cross_set_score(tf.keras.layers.Layer):
+    def __init__(self, head_size=20, num_heads=2):
+        super(cross_set_score, self).__init__()
+        self.head_size = head_size
+        self.num_heads = num_heads
+
+        # multi-head linear function, l(x|W_0), l(x|W_1)...l(x|W_num_heads) for each item feature vector x.
+        # one big linear function with weights of W_0, W_1, ..., W_num_heads outputs head_size*num_heads-dim vector
+        self.linear = tf.keras.layers.Dense(units=self.head_size*self.num_heads,activation='relu',use_bias=False)
 
         # linear function to combine multi-head score maps
         self.conv = tf.keras.layers.Conv2D(filters=1, strides=(1,1), padding='same', kernel_size=(1,1))
 
     def call(self, x):
+        nSet = tf.shape(x)[1]
+        sqrt_head_size = tf.sqrt(tf.cast(self.head_size,tf.float32))
 
-        # linear transofrmation (B*nItem,dim*nHead)
+        # linear transofrmation from (nSet*nItem, nSet, Xdim) to (nSet*nItem, nSet, head_size*num_heads)
         x = self.linear(x)
 
-        # reshape (B*nItem,dim*nHead) to (nHead, B*nItem,dim)
-        x = tf.transpose(tf.reshape(x,[-1,self.nHead,self.dim]),[1,0,2])
+        # reshape (nSet*nItem,head_size*num_heads) to (num_heads, nSet*nItem,head_size)
+        x = tf.transpose(tf.reshape(tf.expand_dims(x,2),[nSet*nItem, nSet, self.num_heads, self.head_size]),[2,0,1,3])        
 
-        # inner products between all pairs of items, outputing (nhead, nSet*nItem, nSet*nItem)-score map
-        inner_prods = tf.matmul(x,tf.transpose(x,[0,2,1]))/tf.sqrt(tf.cast(self.dim,tf.float32))
-
-        # reshape (nHead, nSet*nItem, nSet*nItem,1)
-        inner_prods = tf.expand_dims(inner_prods,-1)
-
-        # sum up score-map in each block of size (nItem, nItem) using conv2d with weights of ones
-        # outputing (nHead, nSet, nSet, 1)-score map
-        scores = tf.keras.layers.Conv2D(filters=1,strides=(nItem,nItem),kernel_size=(nItem,nItem),
-                    trainable=False,use_bias=False,weights=[tf.ones((nItem,nItem,1,1))])(inner_prods)
+        # compute inner products between all pairs of items with cross-set feature (cseft)
+        # Between set #1 and set #2, cseft x[:,0:2,1] and x[:,2:4,0] are extracted to compute inner product when nItem=2
+        # More generally, between set #i and set #j, cseft x[:,j*2:j*2+2,i] and x[:,i*2:i*2+2,j] are extracted.
+        # Outputing (nSet, nSet, num_heads)-score map
+        scores = tf.stack([[tf.reduce_sum(tf.reduce_sum(tf.matmul(x[:,j*nItem:j*nItem+nItem,i],tf.transpose(x[:,i*nItem:i*nItem+nItem,j],[0,2,1]))/sqrt_head_size,axis=1),axis=1)
+                         for i in range(nSet)] for j in range(nSet)])
 
         # devided by the two numbers of items of two sets（NOTE that nItem is SIMPLY fixed）
         scores = scores/nItem/nItem
 
         # linearly combine multi-head score maps
-        # reshape (nHead, nSet, nSet, 1) to (1, nSet, nSet, nHead)
-        scores = tf.transpose(scores,[3,1,2,0])
+        # reshape (nSet, nSet, num_heads) to (1, nSet, nSet, num_heads)
+        scores = tf.expand_dims(scores,0)
         scores = self.conv(scores)
 
         return scores[0,:,:,0]
 #----------------------------        
 
 #----------------------------
-# 全体のネットワーク
+# cross-set feature (cseft)
+class cseft(tf.keras.layers.Layer):
+    def __init__(self, isOnlySelf=False, head_size=20, num_heads=2):
+        super(cseft, self).__init__()
+        self.isOnlySelf = isOnlySelf
+        self.head_size = head_size
+        self.num_heads = num_heads        
+
+        # multi-head linear function, l(x|W_0), l(x|W_1)...l(x|W_num_heads) for each item feature vector x.
+        # one big linear function with weights of W_0, W_1, ..., W_num_heads outputs head_size*num_heads-dim vector
+        self.linear1 = tf.keras.layers.Dense(units=self.head_size*self.num_heads,activation='relu',use_bias=False)
+        self.linear2 = tf.keras.layers.Dense(units=self.head_size*self.num_heads,activation='relu',use_bias=False)
+        self.linear3 = tf.keras.layers.Dense(units=self.head_size*self.num_heads,activation='relu',use_bias=False)
+
+        # linear function to combine multi-head score maps
+        self.conv = tf.keras.layers.Conv2D(filters=1, strides=(1,1), padding='same', kernel_size=(1,1))
+
+    def call(self, x, y):
+        # number of sets
+        nSet = tf.cast(tf.shape(x)[0]/nItem,tf.int32)
+        sqrt_head_size = tf.sqrt(tf.cast(self.head_size,tf.float32))
+
+        # linear transofrmation (nSet*nItem, head_size*num_heads)
+        x = self.linear1(x)
+        y1 = self.linear2(y)
+        y2 = self.linear3(y)
+
+        # reshape (nSet*nItem, head_size*num_heads) to (num_heads, nSet*nItem, head_size)
+        x = tf.transpose(tf.reshape(x,[-1,self.num_heads,self.head_size]),[1,0,2])
+        y1 = tf.transpose(tf.reshape(y1,[-1,self.num_heads,self.head_size]),[1,0,2])
+        y2 = tf.transpose(tf.reshape(y2,[-1,self.num_heads,self.head_size]),[1,0,2])
+
+        # inner products between all pairs of items, outputing (num_heads, nSet*nItem, nSet*nItem)-score map        
+        xy1 = tf.matmul(x,tf.transpose(y1,[0,2,1]))/sqrt_head_size
+
+        # block diagonal matrix containing the block (num_heads, nItem, head_size) of y2 in diagonal elements
+        # outputs (num_heads, nSet*nItem, nSet*head_size)
+        zeros = tf.zeros((self.num_heads, nItem, self.head_size))
+        y2_block_diag = tf.concat([tf.concat([tf.tile(zeros,[1,1,i]),y2[:,i*nItem:(i+1)*nItem],tf.tile(zeros,[1,1,nSet-(i+1)])],axis=2) 
+                        for i in range(nSet)],axis=1) 
+
+        # computing weighted y2 by xy1, outputing (num_heads, nSet*nItem, nSet*head_size)
+        newx = tf.matmul(xy1, y2_block_diag)
+
+        # linearly combine multi-head score maps
+        # reshape (num_heads, nSet*nItem, nSet*head_size) to (1, nSet*nItem, nSet*head_size, num_heads)
+        newx = tf.expand_dims(newx,-1)
+        newx = tf.transpose(newx,[3,1,2,0])
+        newx = newx/nItem
+
+        # combine heads, outputing (1, nSet*nItem, nSet*head_size, 1)
+        newx = self.conv(newx)
+
+        # reshape (1, nSet*nItem, nSet*head_size, 1) to (nSet*nItem, nSet*head_size)
+        newx = newx[0,:,:,0]
+
+        if self.isOnlySelf:
+            # extract only diagonal block (nItem, head_size), outputing (nSet*nItem, head_size)
+            newx = tf.concat([newx[i*nItem:(i+1)*nItem,i*self.head_size:(i+1)*self.head_size] for i in range(nSet)],axis=0) 
+        else:
+            # reshape (nSet*nItem, nSet*head_size) to (nSet*nItem, nSet, head_size)
+            newx = tf.reshape(tf.expand_dims(newx,1),[-1,nSet,self.head_size])
+        
+
+        return newx
+#----------------------------        
+
+#----------------------------
+# design network architecture
 class myModel(tf.keras.Model):
-    def __init__(self, self_num_heads=2):
+    def __init__(self, isAttention=True, num_heads=2):
         super(myModel, self).__init__()
+        self.isAttention = isAttention
 
         # conv, fc, defc & deconv layers
         self.conv1 = tf.keras.layers.Conv2D(filters=baseChn, strides=(2,2), padding='same', kernel_size=(3,3), activation='relu')
         self.conv2 = tf.keras.layers.Conv2D(filters=baseChn*2, strides=(2,2), padding='same', kernel_size=(3,3), activation='relu')
         self.conv3 = tf.keras.layers.Conv2D(filters=baseChn*2, strides=(2,2), padding='same', kernel_size=(3,3), activation='relu')
         self.globalpool = tf.keras.layers.GlobalAveragePooling2D()
-        self.cross_set_score = cross_set_score()
-        self.self_attention = tfa.layers.MultiHeadAttention(head_size=baseChn*2,num_heads=self_num_heads)
+        self.cross_set_score = cross_set_score(head_size=baseChn*2,num_heads=num_heads)
+        self.cseft_enc = cseft(isOnlySelf=True, head_size=baseChn*2,num_heads=num_heads)
+        self.cseft_dec = cseft(head_size=baseChn*2,num_heads=num_heads)
         self.fc = tf.keras.layers.Dense(2,activation='softmax')
 
     def call(self, x):
 
-        # reshape (B, nItem, H, W, C) to (B*nItem, H, W, C)
+        # reshape (nSet, nItem, H, W, C) to (nSet*nItem, H, W, C)
+        nSet = tf.shape(x)[0]
         x = tf.reshape(x,[-1,H,W,C])
         
         # CNN
@@ -167,35 +288,42 @@ class myModel(tf.keras.Model):
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.globalpool(x)
-        
-        # encoder (self-attention)
-        if isSelfAttention:
-            # reshape (B*nItem, D) to (B, nItem, D)
-            x = tf.reshape(x,[-1,nItem,tf.shape(x)[1]])
-            x = self.self_attention([x,x]) # [Queries, Values]
+        cnnfet = x
 
-            # reshape (B, nItem, D) to (B*nItem, D)
-            x = tf.reshape(x,[-1,tf.shape(x)[2]])
+        # Attention
+        if self.isAttention:
+            # encoder (self-set attention)
+            # input: (nSet*nItem, D)
+            # output: (nSet*nItem, D)
+            x = self.cseft_enc(x,x)
+            cseft_enc = x
 
-        # decoder (cross-set transofrmation:cseft)            
+            # decoder (cross-set transofrmation:cseft)
+            # input: (nSet*nItem, D)
+            # output: (nSet*nItem, nSet, D)
+            x = self.cseft_dec(x,x)
+            cseft_dec = x
+
+        else:
+            x = tf.tile(tf.expand_dims(x,1),[1,nSet,1]) 
 
         # cross set matching score
         score = self.cross_set_score(x)
 
         # classificaiton
         output = self.fc(tf.reshape(score,[-1,1]))
-   
-        return output, score
 
-    # 星取表（ラベルyが一致する場合1、異なる場合0）を作成
+        return output, (score, cnnfet, cseft_enc, cseft_dec)
+
+    # convert class labels to cross-set label（if the class-labels are same, 1, otherwise 0)
     def cross_set_label(self, y):
-        # 星取表の行
+        # rows of table
         y_rows = tf.tile(tf.expand_dims(y,-1),[1,tf.shape(y)[0]])
 
-        # 星取表の列        
+        # cols of table       
         y_cols = tf.tile(tf.transpose(tf.expand_dims(y,-1)),[tf.shape(y)[0],1])
 
-        # ラベルが一致する場合1、異なる場合0
+        # if the class-labels are same, 1, otherwise 0
         return 1-tf.abs(y_rows - y_cols)
 
     # def toBinaryLabel(self,y):
@@ -204,73 +332,74 @@ class myModel(tf.keras.Model):
 
     #     return tf.reshape(y,[dNum,-1])
 
-    # 学習
+    # train step
     def train_step(self,data):
         x, y_true = data
         
         with tf.GradientTape() as tape:
             
-            # 予測
-            y_pred, _ = self(x, training=True)
+            # predict
+            y_pred, deb = self(x, training=True)
 
-            # クロスマッチラベルに変換
+            # convert to cross-set label
             y_true = self.cross_set_label(y_true)
             y_true = tf.reshape(y_true,-1)
 
-            # 損失
+            # loss
             loss = self.compiled_loss(y_true, y_pred, regularization_losses=self.losses)
         
-        # 勾配を用いた学習
+        # train using gradients
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
+        #pdb.set_trace()
+
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
-        # 評価値の更新
+        # update metrics
         self.compiled_metrics.update_state(y_true, y_pred)
 
-        # 評価値をディクショナリで返す
+        # return metrics as dictionary
         return {m.name: m.result() for m in self.metrics}
 
-    # 評価
+    # test step
     def test_step(self, data):
         x, y_true = data
         
-        # 予測
+        # predict
         y_pred, _ = self(x, training=False)
 
-        # クロスマッチラベルに変換
+        # convert to cross-set label
         y_true = self.cross_set_label(y_true)
         y_true = tf.reshape(y_true,-1)
 
-        # 損失
+        # loss
         self.compiled_loss(y_true, y_pred, regularization_losses=self.losses)
 
-        # metricsの更新
+        # update metrics
         self.compiled_metrics.update_state(y_true, y_pred)
 
-        # 評価値をディクショナリで返す
+        # return metrics as dictionary
         return {m.name: m.result() for m in self.metrics}
 
-    # 予測
+    # predict step
     def predict_step(self,data):
         x = data
 
-        # 予測
+        # predict
         y_pred, _ = self(x, training=False)
 
-        # 予測
         return y_pred
 
-# モデルの設定
-model = myModel()
+# setting model
+model = myModel(isAttention=isAttention)
 
-# 学習方法の設定
+# setting training, loss, metric to model
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'],run_eagerly=True)
 #----------------------------
 
 #----------------------------
-# 学習
-# 学習したパラメータを保存するためのチェックポイントコールバックを作る
+# train
+# make checkpoint callback to save trained parameters
 checkpoint_path = "setmatching_training/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
@@ -278,10 +407,10 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_o
 
 if isTrain:
 
-    # fitで学習を実行
-    history = model.fit(x_train, y_train, batch_size=10, epochs=10, validation_split=0.2, callbacks=[cp_callback])
+    # execute training
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2, callbacks=[cp_callback])
 
-    # 損失のプロット
+    # plot loss and accuracy
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
     loss = history.history['loss']
@@ -301,19 +430,19 @@ if isTrain:
 
     plt.show()    
 else:
-    # 学習したパラメータの読み込み
+    # load trained parameters
     model.load_weights(checkpoint_path)
 #----------------------------
 
 #----------------------------
-# 学習データに対する評価
+# evaluation with training data
 train_loss, train_acc = model.evaluate(x_train[:1000], y_train[:1000], verbose=0)
 print('Train data loss:', train_loss)
 print('Train data accuracy:', train_acc)
 #----------------------------
 
 #----------------------------
-# 評価データに対する評価
+# evaluation with validation data
 test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
 print('Test data loss:', test_loss)
 print('Test data accuracy:', test_acc)
